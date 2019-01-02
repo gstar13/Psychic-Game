3b05d2604aea7e9fd34f39fd661727b69a3094e6
GStar13 <ginazavala004@gmail.com>
2019-01-02 09:58:04 -0800

    pushing working game

--- assets/javascript/game.js/game.js
+++ assets/javascript/game.js/game.js
@@ -1,39 +1,71 @@
 // creates an array that lists all of the options for the computer to guess from
-var computerChoices = [a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+var computerChoices = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 //creates variables to hold the number of wins and losses
-
 var wins = 0;
 var losses = 0;
-var guesses = 0;
-var guessesleft = 10;
+var guessesLeft = 9; 
+var guesses= 9;
+var lettersGuessed = [];
+var rightAnswer;
 
-//creates variables that hold references to the places in the html
-//where we want to display things
 
-var directionsText = document.getElementById ("directions-text");
-var userChoiceText = document.getElementById ("userchoice-text");
-var computerChoiceText = document.getElementById ("computerchoice-text");
-var winsText = document.getElementById ("wins-text");
-var lossesText = document.getElementById ("losses-text");
-var tiesText = document.getElementById ("ties-text");
-var guessesLeft = document.getElementById ("guessesleft-text");
-var guessesSoFar = document.getElementById ("guessessofar-text");
+ //randomly chooses from options array
+ var computerGuess= function () {
+     rightAnswer = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+     console.log(rightAnswer);
+ };
+ var newGuesses = function () {
+    document.getElementById ("lettersGuessed").innerHTML = "Guesses So Far: " + lettersGuessed.join(",");
+ };
+  var guessLeft = function() {
+      document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+  };
+
+  var game = function () {
+      lettersGuessed = [];
+      guessesLeft = 9;
+      computerGuess();
+      guessLeft();
+      newGuesses();
+
+  }
+
 
 //this function will run anytime the user presses a key
 document.onkeyup = function(event) {
     //determines which key was pressed
     var userGuess= event.key;
-     //randomly chooses from options array
-    var computerGuess= computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
-     //alerts the key the user pressed (userGuess)
+    //alerts the key the user pressed (userGuess)
     alert("User guess: " + userGuess);
-    //alerts the computer's guess
-    alert( "Computer's guess" + computerGuess);
-    
-    if (userGuess===computerGuess) {
-        alert(" You Won");
-        wins++; guessesleft--;
-
+    guessesLeft--;
+    console.log(guessesLeft);
+    lettersGuessed.push(userGuess);
+    newGuesses();
+    guessLeft();
+    if (guessesLeft>0) {
+        if (userGuess===rightAnswer)  {
+            alert(" You Won");
+            wins++; 
+            document.getElementById("wins").innerHTML = "wins:" + wins;
+            game();
     }
+
+} else if (guessesLeft == 0){
+    alert("Nope");
+    losses++;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    game();
 }
+};
+   
+    
+     
+     
+   
+      
+       
+
+   
+    
